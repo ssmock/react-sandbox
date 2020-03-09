7a8ad024ba947e39d252bc9e591ef7a98ae6f00a
steve <ssmock@gmail.com>
2020-03-09 10:17:57 -0400

    Refine methods for picking from bags

--- src/randomness/bag.test.ts
+++ src/randomness/bag.test.ts
@@ -8,27 +8,27 @@ describe("Bags.make", () => {
 
         expect(b.items.count()).toEqual(3);
     });
-    
+
     it("should ignore duplicates of primative values", () => {
         const b = Bags.make([1, 2, 2, 1]);
-    
+
         expect(b.items.count()).toEqual(2);
     });
-    
+
     it("should not ignore structurally equal duplicates", () => {
         const b = Bags.make([
-            { a: 1 }, 
-            { a: 2 }, 
-            { a: 2 }, 
+            { a: 1 },
+            { a: 2 },
+            { a: 2 },
             { a: 1 }]);
-    
+
         expect(b.items.count()).toEqual(4);
     });
 });
 
-xdescribe("Bags.takeOne", () => {
+describe("Bags.takeOne", () => {
     beforeAll(() => {
-        Rng.seed("Bags.take");
+        Rng.seed("Bags.takeOne");
     });
 
     it("should take an item at random", () => {
@@ -38,6 +38,89 @@ xdescribe("Bags.takeOne", () => {
         const taken = Bags.takeOne(b);
 
         expect(taken.bag.items.count()).toBe(9);
-        expect(taken.selection).toEqual(3);
+        expect(taken.selection).toEqual(4);
+    });
+
+    it("should take several items at random", () => {
+        const items = Range(0, 10).toArray();
+        const b = Bags.make(items);
+
+        const t1 = Bags.takeOne(b);
+        expect(t1.bag.items.count()).toBe(9);
+        expect(t1.selection).toEqual(1);
+
+        const t2 = Bags.takeOne(t1.bag);
+        expect(t2.bag.items.count()).toBe(8);
+        expect(t2.selection).toEqual(7);
+
+        const t3 = Bags.takeOne(t2.bag);
+        expect(t3.bag.items.count()).toBe(7);
+        expect(t3.selection).toEqual(6);
+    });
+
+    it("should exhaust a bag", () => {
+        const items = Range(0, 10).toArray();
+        const b = Bags.make(items);
+
+        const finallyTaken =
+            Range(0, 9).reduce((taken, i) => {
+                return Bags.takeOne(taken.bag);
+            }, Bags.takeOne(b));
+
+        expect(finallyTaken.bag.items.count()).toBe(0);
     });
 });
+
+describe("Bags.takeMany", () => {
+    beforeAll(() => {
+        Rng.seed("Bags.takeMany");
+    });
+
+    it("should take several items at random, leaving some left", () => {
+        const items = Range(0, 10).toArray();
+        const b = Bags.make(items);
+
+        const taken = Bags.takeMany(5, b);
+
+        expect(taken.bagEmpty).toEqual(false);
+        expect(taken.deficit).toEqual(0);
+        expect(taken.selections.count()).toEqual(5);
+        expect(taken.selections.toArray()).toEqual([
+            7, 9, 1, 0, 8
+        ]);
+        expect(taken.bag.items.count()).toBe(5);
+    });
+
+    it("should exhaust a bag", () => {
+        const items = Range(0, 10).toArray();
+        const b = Bags.make(items);
+
+        const taken = Bags.takeMany(10, b);
+
+        expect(taken.bag.items.count()).toBe(0);
+    });
+
+    it("indicate a deficit", () => {
+        const items = Range(0, 10).toArray();
+        const b = Bags.make(items);
+
+        const taken = Bags.takeMany(15, b);
+
+        expect(taken.bag.items.count()).toBe(0);
+        expect(taken.selections.count()).toBe(10);
+        expect(taken.bagEmpty).toBe(true);
+        expect(taken.deficit).toBe(5);
+    });
+});
+
+xdescribe("Bags.peek", () => {
+    it("should have tests", () => {
+        fail();
+    });
+})
+
+xdescribe("Bags.peekMany", () => {
+    it("should have tests", () => {
+        fail();
+    });
+})
--- src/randomness/bag.ts
+++ src/randomness/bag.ts
@@ -1,4 +1,5 @@
-import { Seq, Set, Range } from "immutable";
+import { Seq, Set, Range, List } from "immutable";
+import { Rng } from "./rng";
 
 export type Bag<T> = {
     items: Set<T>;
@@ -6,12 +7,15 @@ export type Bag<T> = {
 
 export type FromBag<T> = {
     bag: Bag<T>;
-    selection: T;
+    selection: T | null;
+    bagEmpty: boolean;
 };
 
 export type ManyFromBag<T> = {
     bag: Bag<T>;
-    selections: Seq.Indexed<T>;
+    selections: List<T>;
+    bagEmpty: boolean;
+    deficit: number;
 }
 
 export const Bags = {
@@ -22,28 +26,82 @@ export const Bags = {
     },
 
     takeOne<T>(bag: Bag<T>): FromBag<T> {
-        throw Error("Not implemented");
+        const arr = bag.items.toArray();
+
+        if (!arr.length) {
+            return {
+                bag,
+                selection: null,
+                bagEmpty: true
+            }
+        }
+
+        const i = Rng.pick(0, arr.length - 1);
+        const selection = arr[i];
+
+        const newBag = Bags.make([
+            ...arr.slice(0, i),
+            ...arr.slice(i + 1, arr.length)
+        ]);
+
+        return {
+            selection,
+            bag: newBag,
+            bagEmpty: false
+        };
     },
 
-    takeMany<T>(bag: Bag<T>, count: number): ManyFromBag<T> {
+    takeMany<T>(count: number, bag: Bag<T>): ManyFromBag<T> {
         return Range(0, count).reduce((agg: ManyFromBag<T>, i: number): ManyFromBag<T> => {
             const fromBag = Bags.takeOne(agg.bag);
 
+            if (fromBag.selection === null) {
+                return {
+                    bag: agg.bag,
+                    selections: agg.selections,
+                    bagEmpty: true,
+                    deficit: agg.deficit + 1
+                };
+            }
+
             return {
-                bag: {
-                    items: agg.bag.items.remove(fromBag.selection)
-                },
-                selections: Seq<T>([])
+                bag: fromBag.bag,
+                selections: agg.selections.push(fromBag.selection),
+                bagEmpty: false,
+                deficit: 0
             };
         }, {
             bag,
-            selections: Seq<T>([])
+            selections: List<T>([]),
+            bagEmpty: false,
+            deficit: 0
         });
     },
 
-    put<T>(bag: Bag<T>, ...items: T[]): Bag<T> {
+    put<T>(items: T[], bag: Bag<T>): Bag<T> {
         return {
             items: bag.items.concat(items)
+        };
+    },
+
+    peekOne<T>(bag: Bag<T>): FromBag<T> {
+        const taken = Bags.takeOne(bag);
+
+        return {
+            bag,
+            selection: taken.selection,
+            bagEmpty: taken.bagEmpty
+        };
+    },
+
+    peekMany<T>(count: number, bag: Bag<T>): ManyFromBag<T> {
+        const taken = Bags.takeMany(count, bag);
+
+        return {
+            bag,
+            selections: taken.selections,
+            bagEmpty: taken.bagEmpty,
+            deficit: taken.deficit
         };
     }
 };
